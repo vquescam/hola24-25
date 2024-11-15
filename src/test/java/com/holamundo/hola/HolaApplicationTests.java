4afdb0e42e185d74261657db0fdbabf81bbf41f5
Vicente Quesada <v.quesada@Vicentes-MacBook-Pro.local>
2024-11-15 13:35:33 +0100

    Initial commit

---
+++ src/test/java/com/holamundo/hola/HolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holamundo.hola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
